d99a6555102a3b6f4bea494ba7d88b9d31835384
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2019-10-11 17:35:42 -0500

    Initial

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jpcano1/Documents/Programación/Python/simple_dj_docker/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
